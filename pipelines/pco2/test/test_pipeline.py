c239bdfd71bacbeab64ea744f03e7a93c3f76f30
jmcvey3 <53623232+jmcvey3@users.noreply.github.com>
2023-10-05 09:40:16 -0700

    Fix trigger

--- pipelines/pco2/test/test_pipeline.py
+++ pipelines/pco2/test/test_pipeline.py
@@ -8,7 +8,7 @@ def test_pco2_pipeline():
     config = PipelineConfig.from_yaml(config_path)
     pipeline = config.instantiate_pipeline()
 
-    test_file = "pipelines/pco2/test/data/input/pco2_20231003_060000.raw.nc"
+    test_file = "pipelines/pco2/test/data/input/pco2_raw_20231003_060000.nc"
     expected_file = "pipelines/pco2/test/data/expected/pco2.mcrl_pier-battelle_seaology_1-3hr.a1.20231003.060000.nc"
 
     dataset = pipeline.run([test_file])
